d31993aa57b55e065c228c3293396d2234449c8a
Ditte Lykke Hansen <dittehansen143@gmail.com>
2024-03-06 22:45:10 +0100

    dashboard setup

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import './Dashboard.css';
+import DashboardItems from './DashboardItems';
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const dashboardItems = [
+    {
+      id: 1,
+      title: 'Ordre',
+      description: 'Her skal der stå hvor mange ordre.',
+    },
+    {
+      id: 2,
+      title: 'Profit',
+      description:
+        'Her skal der stå hvor meget profit, der er lavet med denne million forretning.',
+    },
+  ];
 
-export default Dashboard
+  return (
+    <div className="container customMargin">
+      <h1 className="mt-5 Cronus-font">Dashboard</h1>
+      <div className="row">
+        {dashboardItems.map((dashboardItem) => (
+          <div key={dashboardItem.id} className="col-6 col-lg-3 mb-5 dashItem">
+            <DashboardItems dashboardItem={dashboardItem} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/Dashboard/DashboardItems.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Dashboard.css';
+import { Card } from 'react-bootstrap';
+
+
+const DashboardItems = ({dashboardItem}) => {
+    console.log(dashboardItem)
+  return (
+    <Card className="card-custom dashCard">
+      <Card.Body className='dashCardBody'>
+        <Card.Title className="Cronus-font">
+          {dashboardItem.title}
+        </Card.Title>
+        <Card.Text>
+          {dashboardItem.description}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default DashboardItems;
